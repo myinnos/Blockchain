d1639e513bb1fb00a915f61570bac893f034d741
MyInnos <prabhakar_thota@live.com>
2018-07-01 00:12:33 +0530

    defined create_block function

--- blockchain.py
+++ blockchain.py
@@ -9,17 +9,24 @@ Created on Sat Jun 30 23:53:41 2018
 # To be installed:
 # Flask==0.12.2: pip install Flask==0.12.2
 
-# Importing the libraries
 import datetime
 import hashlib
 import json
 from flask import Flask, jsonify
 
-# Part 1 - Building a Blockchain
+# Building a Blockchain
 
 class Blockchain:
     
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
+        
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp' : str(datetime.datetime.now()),
+                 'proof' : proof,
+                 'previous_hash' : previous_hash}
+        self.chain.append(block)
+        return block
 

3de1f1f2e287982092121d2d326ad87876a98b22
MyInnos <prabhakar_thota@live.com>
2018-07-01 00:28:36 +0530

    defined proof_of_work function

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jun 30 23:53:41 2018
-
 @author: myinnos
 """
 
@@ -32,3 +30,14 @@ class Blockchain:
 
     def get_previous_block(self):
         return self.chain[-1]
+    
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof

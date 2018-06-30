a1e4a801816bb51c61a733330bde387f5b084c93
MyInnos <prabhakar_thota@live.com>
2018-07-01 01:17:17 +0530

    defined api functions

--- blockchain.py
+++ blockchain.py
@@ -65,14 +65,14 @@ class Blockchain:
 # Mining our Blockchain
 
 # Web App
- app = Flask(__name__)
-        
+app = Flask(__name__) 
+ 
 # Blockchain
 blockchain = Blockchain()
 
 # Mining a new block
 @app.route('/mine_block', methods = ['GET'])
-def mine_block()
+def mine_block():
     previous_block = blockchain.get_previous_block()
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
@@ -92,19 +92,18 @@ def get_chain():
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
+# Checking if the Blockchain is valid
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid.'}
+    else:
+        response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
+    return jsonify(response), 200
 
-
-
-
-
-
-
-
-
-
-
-        
-        
+# Running the app
+app.run(host = '0.0.0.0', port = 8899)
         
         
         

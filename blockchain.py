6d9fc681c397f62c5bab3720138902953db90720
MyInnos <prabhakar_thota@live.com>
2018-07-01 00:02:40 +0530

    intilize class and varibales

--- blockchain.py
+++ blockchain.py
@@ -6,3 +6,20 @@ Created on Sat Jun 30 23:53:41 2018
 @author: myinnos
 """
 
+# To be installed:
+# Flask==0.12.2: pip install Flask==0.12.2
+
+# Importing the libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part 1 - Building a Blockchain
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+
